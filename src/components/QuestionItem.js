fde82e3aa11b24f2f50de3e0426d7cee206e5673
Zhongming <atenliao@gmail.com>
2023-06-08 01:50:18 -0700

    updated components

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+  // const [isCorrectIndex, setIsCorrectIndex] = useState(correctIndex)
+  // const [isDelete, setIsDelete] = useState(false)
+  function handleDeleteClick(){
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method:'DELETE',
+    })
+    .then(res=> res.json())
+    .then(()=> onDeleteQuestion(question))
+  }
+
+  function handleUpdate(event){
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method:'PATCH',
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify({
+        correctIndex: event.target.value
+      })
+    })
+    .then(res=> res.json())
+    .then((question) => onUpdateQuestion(question))
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +38,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleUpdate}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
